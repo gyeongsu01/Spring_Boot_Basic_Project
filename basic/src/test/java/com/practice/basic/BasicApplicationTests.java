5757ef770919672e619a9b1e1909abde1986b610
gyeongsu01 <rudtn021371@gmail.com>
2025-09-16 19:25:00 +0900

    Spring Boot Basic Project 초기 생성

---
+++ basic/src/test/java/com/practice/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
